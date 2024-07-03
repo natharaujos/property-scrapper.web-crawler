d55fb6b912e0c165c91705476ec596285878ee0d
Nathan Araujo <nathansaraujo191@gmail.com>
2024-07-03 20:22:00 -0300

    adding gitignore file and database connection

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -1,7 +1,35 @@
 import asyncio
 from ticle_scrapper import TicleScrapper
+import mysql.connector
 
-ticle_scrapper = TicleScrapper()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(ticle_scrapper.find_elements_to_interact())
-loop.run_until_complete(ticle_scrapper.execute_scrapping())
+try:
+    # Crie uma conexão com o banco de dados
+    cnx = mysql.connector.connect(user='admin', password='#Mpn2012nicker',
+                                  host='localhost',
+                                  database='property-scrapper-schema')
+
+    cursor = cnx.cursor()
+
+    add_data = ("INSERT INTO properties "
+                   "(name, value, description, link) "
+                   "VALUES (%s, %s, %s, %s)")
+
+    ticle_scrapper = TicleScrapper()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ticle_scrapper.find_elements_to_interact())
+    result = loop.run_until_complete(ticle_scrapper.execute_scrapping())
+
+    for r in result:
+        data = (r.name, r.value, r.description, r.link)
+        cursor.execute(add_data, data)
+
+    cnx.commit()
+
+except mysql.connector.Error as err:
+    print(f"Something went wrong: {err}")
+
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'cnx' in locals():
+        cnx.close()
